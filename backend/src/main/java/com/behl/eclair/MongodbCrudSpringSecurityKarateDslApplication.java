f6aeb9bd9fe8e3442fba3afbf4f306c352b54ebd
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2022-03-10 13:35:49 +0530

    backend-web-app init commit

---
+++ backend/src/main/java/com/behl/eclair/MongodbCrudSpringSecurityKarateDslApplication.java
@@ -0,0 +1,13 @@
+package com.behl.eclair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbCrudSpringSecurityKarateDslApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodbCrudSpringSecurityKarateDslApplication.class, args);
+	}
+
+}
