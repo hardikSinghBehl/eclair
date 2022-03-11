479134bfeb240f03ae1e4a1154a0b8c20128d96c
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2022-03-11 08:47:32 +0530

    removing parallel test execution config

--- integration-tests-karate/src/test/java/com/behl/it/eclair/IntegrationTest.java
+++ integration-tests-karate/src/test/java/com/behl/it/eclair/IntegrationTest.java
@@ -11,7 +11,7 @@ class IntegrationTest {
 
 	@Test
 	void testParallel() {
-		Results results = Runner.path("classpath:com/behl/it/eclair/feature").parallel(5);
+		Results results = Runner.path("classpath:com/behl/it/eclair/feature").parallel(1);
 		assertEquals(0, results.getFailCount(), results.getErrorMessages());
 		Runner.runFeature("classpath:com/behl/it/eclair/helper/DeleteUser.feature", null, true);
 	}
